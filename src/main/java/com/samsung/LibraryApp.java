678a276a1c0fc8c037f6cfadf3c0002b8523879c
anepretimovdiasoft <94528503+anepretimovdiasoft@users.noreply.github.com>
2022-03-23 19:23:07 +0300

    H2, liquibase

--- src/main/java/com/samsung/LibraryApp.java
+++ src/main/java/com/samsung/LibraryApp.java
@@ -1,15 +1,29 @@
 package com.samsung;
 
+import org.h2.tools.Console;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+
+import java.sql.SQLException;
+
+// url h2 консоли: http://localhost:8080/h2-console
+// url базы: jdbc:h2:mem:testdb
 
 @SpringBootApplication
 public class LibraryApp {
 
     public static void main(String[] args) {
 
+        //Запуск SpringBoot-приложения и получение контекста
         ConfigurableApplicationContext context = SpringApplication.run(LibraryApp.class, args);
+
+        try {
+            //Консоль для визуализации бд в браузере
+            Console.main(args);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
     }
 }
