7b212e2555c9962130a5d995e06a0d561a80a748
anepretimovdiasoft <94528503+anepretimovdiasoft@users.noreply.github.com>
2022-03-25 16:21:23 +0300

    controller, dto, unit-test

--- src/main/java/com/samsung/LibraryApp.java
+++ src/main/java/com/samsung/LibraryApp.java
@@ -1,15 +1,8 @@
 package com.samsung;
 
-import com.samsung.dao.BookDao;
-import com.samsung.domain.Book;
-import com.samsung.service.LibraryDemo;
-import org.h2.tools.Console;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
-
-import java.sql.SQLException;
-import java.util.List;
 
 // url h2 консоли: http://localhost:8080/h2-console
 // url базы: jdbc:h2:mem:testdb
@@ -21,11 +14,6 @@ public class LibraryApp {
 
         //Запуск SpringBoot-приложения и получение контекста
         ConfigurableApplicationContext context = SpringApplication.run(LibraryApp.class, args);
-
-        LibraryDemo libraryDemo = context.getBean(LibraryDemo.class);
-        libraryDemo.libDemo();
-
-        context.close();
 
         /*try {
             //Консоль для визуализации бд в браузере
